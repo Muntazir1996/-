33767ab55f0bc47eb7fbf2029f3057cc61400fa5
منتظر حيدر هلال <106447183+Muntazir1996@users.noreply.github.com>
2022-08-19 20:02:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+var a = document.getElementById("aa");
+function playMusicx(){
+	a.play();
+}
+ 
+
+
+var b = document.getElementById("bb");
+function playMusicb(){
+	b.play();
+}
+ 
+var c = document.getElementById("cc");
+function playMusicc(){
+	c.play();
+}
+var d = document.getElementById("d");
+function playMusicd(){
+	d.play();
+}
+
+var e = document.getElementById("e");
+function playMusice(){
+	e.play();
+}
+
+var f = document.getElementById("f");
+function playMusicf(){
+	f.play();
+}
+
+var g = document.getElementById("g");
+function playMusicg(){
+	g.play();
+}
+
+ 
+ 
